23871a64ee959f801f06001dc862d909c158b08f
A01703397 <69997625+Yam2020@users.noreply.github.com>
2020-10-02 17:54:01 -0500

    Avance_5

--- Proyecto.py
+++ Proyecto.py
@@ -1,16 +1,15 @@
 #Yamile Saray García Rivera
 #A01703397
-#Avance_4_proyecto
+#Avance_5_proyecto
 
-#Nota Futura: Cuando me enseñen "listas" planeao agrupar mis respuestas, para simplificar el codigo.
-
-#Hola mundo 
-
-#Lista de 7 preguntas (notas)
 #Funciones
 #If else, para saber si la respuesta es correcta o incorrecta
+#Funciones 
 #Ciclo while la cantidad de preguntas sean < = 7
+#Lista de 7 preguntas (notas)
 
+
+#Funciones "mostrar_pregunta" y "Evaluar respuesta"; son para nuestro primer nivel. 
 
 #Funcion para mostrar preguntas
 #No regresa ningun valor (return)
@@ -115,9 +114,40 @@ def evaluar_respuesta (numero_pregunta, respuesta):
             
     
     return (booRespuesta)
-            
-            
-        
+
+
+##################################################
+#Funciones para Nivel 2. Donde tendremos dos funciones "crear_pregunta" y "crear_respuestas" utilizando listas. 
+
+#Funcion donde almacena las pregunta al usuario. Son cinco en total. 
+def crear_pregunta():
+    arrPreguntas = []
+    arrPreguntas.append ("Pregunta 1: ¿Duración de la nota redonda? ")
+    arrPreguntas.append ("Pregunta 2: ¿Duración de la nota blanca? ")
+    arrPreguntas.append ("Pregunta 3: ¿Duración de la nota negra? ")
+    arrPreguntas.append ("Pregunta 4: ¿Duración de la nota corchea? ")
+    arrPreguntas.append ("Pregunta 5: ¿Duración de la nota Semicorchea? ")
+    
+    return arrPreguntas
+
+#Función donde almacena las respuestas correctas. 
+def crear_respuestas():
+    arrRespuestas = []
+    arrRespuestas.append ("Cuatro tiempos")
+    arrRespuestas.append ("Dos tiempos")
+    arrRespuestas.append ("Un tiempo")
+    arrRespuestas.append ("Medio tiempo")
+    arrRespuestas.append ("Un cuarto")
+     
+    return arrRespuestas
+
+#Creamos Preguntas en formar de variables. 
+arreglosPreguntasNivel2 = crear_pregunta ()
+arreglosRespuestaNivel2 = crear_respuestas ()
+
+#############################################
+#Bievenida al usuario. 
+
 #Prueba 1     
 print ("Bienvenido a Aprende Música Facil")
 print ("***")
@@ -139,4 +169,31 @@ while numero_de_pregunta <= 7:
     
     #Ahora llamamos la variable que contiende la función evaluar_respuesta, para evaluar si es verdadera prosiga con la siguiente pregunta. 
     if booAlmacena == True:
-        numero_de_pregunta = numero_de_pregunta + 1     
+        numero_de_pregunta = numero_de_pregunta + 1
+        
+#########################################################
+#Segundo Nivel, donde llamaremos a nuestras funciones "crear_pregunta" y "crear_respuestas"
+
+#Lo inicializamos en cero, así empieza nuestra lista. 
+print ("***")
+print ("***")
+print ("")
+print ("Segundo Nivel")
+contador = 0 
+limite_maximo = (len(arreglosPreguntasNivel2) - 1)  #arreglosPreguntasNivel2 su longitud es igual a 2, porque cuenta la cantidad de elementos en la lista.  
+
+#Máximo igual a 1: en este caso tenemos nuestro límite de 2 preguntas. 
+while contador <= limite_maximo:
+    Pregunta = arreglosPreguntasNivel2 [contador]
+    
+    print (Pregunta)
+    Respuesta = arreglosRespuestaNivel2 [contador]
+    dato_entrada = input("Introduce la respuesta correcta: ")
+    
+    if dato_entrada == Respuesta:
+        print ("Correcto")
+        contador = contador + 1 
+        
+    else:
+        print ("Incorrecto")
+        
